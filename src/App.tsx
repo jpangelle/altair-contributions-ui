00f086ff7112d376bbf9193753a5288772c72fcc
JP Angelle <jp@angelle.me>
2021-09-19 23:09:15 -0500

    fix: add height to loader

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ export const App = () => {
         <div
           style={{
             width: '100%',
-            height: '100%',
+            height: '300px',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
